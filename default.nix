5e7dca4da891515cf5911eaec6d65c9ebaa9eaee
Joshua Price <2855417+ElderEphemera@users.noreply.github.com>
2021-07-28 13:41:01 -0400

    Factor out pinned nixpkgs

--- default.nix
+++ default.nix
@@ -1,12 +1,4 @@
-let
-  pinnedPkgs = import (builtins.fetchTarball {
-    name = "nixos-unstable-2021-07-12";
-    url = "https://github.com/nixos/nixpkgs/archive/45fc7d4a35c.tar.gz";
-    sha256 = "1z2afrpdpyk9p120xc9vx79xz61lviz2csalzvikypq85drs1hgd";
-  }) {};
-in
-
-{ pkgs ? pinnedPkgs
+{ pkgs ? import nix/pinned-nixpkgs.nix
 , checkLinks ? true
 }:
 
