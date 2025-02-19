2c0f789ed23c04ede2772f69c5059d31b4931767
Joshua Price <git@elderephemera.com>
2025-02-19 07:09:40 -0500

    Update pinned nixpkgs to nixos-24.05

--- nix/pinned-nixpkgs.nix
+++ nix/pinned-nixpkgs.nix
@@ -1,5 +1,5 @@
 import (builtins.fetchTarball {
-    name = "nixos-22.05";
-    url = "https://github.com/nixos/nixpkgs/archive/ce6aa13369b.tar.gz";
-    sha256 = "0d643wp3l77hv2pmg2fi7vyxn4rwy0iyr8djcw1h5x72315ck9ik";
+    name = "nixos-24.05";
+    url = "https://github.com/nixos/nixpkgs/archive/63dacb46bf9.tar.gz";
+    sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
 }) {}
